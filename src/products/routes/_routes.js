89c4e4b0a495dbfe0efecebd43df82bea8ec2983
Developer <chandankumar75707@gmail.com>
2024-10-21 11:23:13 +0530

    remove file

--- src/products/routes/_routes.js
+++ src/products/routes/_routes.js
@@ -2,7 +2,7 @@ const productController = require("../controllers/_product_controller");
 const categoryController = require("../controllers/_categories_controller")
 const { upload } = require("../../middleware/_files_upload");
 module.exports = (app) => {
-  app.post("/api/v1/create_product", upload, productController.createProduct);
+  app.post("/api/v1/create_product", productController.createProduct);
   app.get("/api/v1/product_list", productController.getProductList);
   app.get("/api/v1/product_details", productController.getProductDetails);
   app.post("/api/v1/create_category", categoryController.createProductCategory);
